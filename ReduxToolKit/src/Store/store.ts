66689a4ec4886e094841e2a94b6d834db8632f45
Kkkermit <jeaton446@gmail.com>
2024-03-27 14:48:08 +0000

    update: Updated the import for createStore so its not longer deprecated

--- ReduxToolKit/src/Store/store.ts
+++ ReduxToolKit/src/Store/store.ts
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from 'redux';
 import { todoReducer } from "../Reducers/taskReducer";
 
 const store = createStore(todoReducer);
